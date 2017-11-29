772b2646322dd7e3b1397f714d6cf541e622c219
Colin Targonski <ctargon@g.clemson.edu>
2017-11-29 12:30:34 -0500

    hedgehog notebook added, notch subset test

--- scripts/subset_gene_test.py
+++ scripts/subset_gene_test.py
@@ -24,7 +24,7 @@ def sanitize(gene_str):
 # create_new_combos takes in a file string that is an accuracy file with a list of genes
 # separated by a tab, followed by the accuracy for that list. it returns a dictionary
 # of new combinations with one extra gene appended that was not previously in the list
-def create_new_combos(file, genes):
+def create_new_combos_from_file(file, genes):
 	combos = []
 	# for subset in itertools.combinations(genes, 2):
 	# 	combos.append(subset)
@@ -45,6 +45,14 @@ def create_new_combos(file, genes):
 	combos = [tuple(g) for g in combos]
 	return dict.fromkeys(combos)
 
+# create every possible combination
+def create_raw_combos(genes, i):
+	combos = []
+	for c in itertools.combinations(genes, i):
+		combos.append(c)
+
+	return dict.fromkeys(combos)
+
 # create a new dataset in the datasets directory that contains the given genes in sub_gene
 def create_subset(sub_gene, tot_gene_lists):
 	gene_count_dict = dict()
@@ -53,13 +61,13 @@ def create_subset(sub_gene, tot_gene_lists):
 
 	gene_indexes = []
 	for i in range(len(sub_gene)):
-		gene_indexes.append(np.argwhere(tot_gene_lists == sub_gene[i]))
+		gene_indexes.append(np.argwhere(tot_gene_lists == sub_gene[i])[0])
 
-	if os.path.isdir('../datasets/GTEx_Hedgehog'):
-		shutil.rmtree('../datasets/GTEx_Hedgehog')
+	if os.path.isdir('../datasets/GTEx_Notch'):
+		shutil.rmtree('../datasets/GTEx_Notch')
 
-	if not os.path.isdir('./datasets/GTEx_Hedgehog'):
-		os.mkdir('../datasets/GTEx_Hedgehog')
+	if not os.path.isdir('./datasets/GTEx_Notch'):
+		os.mkdir('../datasets/GTEx_Notch')
 
 	tissues = os.listdir("../datasets/GTEx_Data")
 
@@ -67,45 +75,48 @@ def create_subset(sub_gene, tot_gene_lists):
 	for i in range(len(tissues)):#loop through all tissue types
 	    tissue_samples = os.listdir("../datasets/GTEx_Data/"+ tissues[i])#get dat files/samples
 
-	    if not os.path.isdir('./datasets/GTEx_Hedgehog/' + tissues[i]):
-	    	os.mkdir('../datasets/GTEx_Hedgehog/' + tissues[i])
+	    if not os.path.isdir('./datasets/GTEx_Notch/' + tissues[i]):
+	    	os.mkdir('../datasets/GTEx_Notch/' + tissues[i])
 
 	    for j in range(gene_count_dict[tissues[i]]):#loop through dat files
 	        sample = np.fromfile("../datasets/GTEx_Data/"+ tissues[i]+'/' + tissue_samples[j], dtype=np.float32)#get 1 sample
 
 		sub_sample = np.zeros(len(gene_indexes))
-		for r in range(len(gene_indexes)):#loop through random indexes
+		for r in range(len(gene_indexes)):#loop through indexes
 			sub_sample[r] = sample[gene_indexes[r]]
 
 		sub_sample = sub_sample.astype(np.float32)
 		#print(len(random_sample))
-		sub_sample.tofile("../datasets/GTEx_Hedgehog/"+ tissues[i] + '/' + tissue_samples[j])
+		sub_sample.tofile("../datasets/GTEx_Notch/"+ tissues[i] + '/' + tissue_samples[j])
 		
 
 # load the hedgehog data
-sub = np.load('../datasets/hallmark_numpys/HALLMARK_HEDGEHOG_SIGNALING.npy')
+sub = np.load('../datasets/hallmark_numpys/HALLMARK_NOTCH_SIGNALING.npy')
 genes = sub[:,1].tolist()
 
-for i in xrange(6, 36):
+# collect the total list of genes in the GTEx dataset
+gtex_str_data = np.load('../datasets/gtex_gct_data_string.npy')
+total_gene_list = gtex_str_data[1:,1]
+
+for i in xrange(1, 4):
 
 	print('--------ITERATION ' + str(i) + '--------')
 	# read in the previous accuracy file
-	gene_dict = create_new_combos('../logs/hedgehog_' + str(i) + '_gene_accuracy.txt', genes)
+	#gene_dict = create_new_combos_from_file('../logs/hedgehog_' + str(i) + '_gene_accuracy.txt', genes)
+
+	gene_dict = create_raw_combos(genes, i)
 
 	# collect the data from the hedgehog file
 	data = sub[:,2:] # raw data is in 2
 
 	# create files to write to, specify neural net architecture
-	files = ['hedgehog_' + str(i + 1) + '_gene_accuracy.txt']
+	files = ['notch_' + str(i + 1) + '_gene_accuracy.txt']
+	
 	h1 = [1024]
 	h2 = [1024]
 	h3 = [1024]
 
-	# collect the total list of genes in the GTEx dataset
-	gtex_str_data = np.load('../datasets/gtex_gct_data_string.npy')
-	total_gene_list = gtex_str_data[1:,1]
-
-	fp = open('../logs/' + files[0], 'w')
+	fp = open('../logs/notch/' + files[0], 'w')
 
 	for key in gene_dict:
 		# retrieve the new combination of genes and create a new dataset containing the specified features
@@ -113,10 +124,10 @@ for i in xrange(6, 36):
 		create_subset(combo, total_gene_list)
 
 		# partition the newly created datset into a training and test set
-		os.system('python ../data_scripts/create-sets.py -d gtex -p ../datasets/GTEx_Hedgehog ' + ' -t 70 -r 30 ')
+		os.system('python ../data_scripts/create-sets.py -d gtex -p ../datasets/GTEx_Notch ' + ' -t 70 -r 30 ')
 
 		# run the neural network architecture to retrieve an accuracy based on the new dataset
-		acc = subprocess.check_output('python ../models/nn_gtex.py --n_input ' + str(i + 1) + \
+		acc = subprocess.check_output('python ../models/nn_gtex.py --n_input ' + str(i) + \
 			' --n_classes 53 --batch_size 256 --lr 0.001 --epochs 75 --h1 ' + str(h1[0]) + ' --h2 ' + str(h2[0]) + ' --h3 ' + str(h3[0]), shell=True)
 
 		print('iteration ' + str(i) + ' ' + str(combo) + '\t' + str(acc))
