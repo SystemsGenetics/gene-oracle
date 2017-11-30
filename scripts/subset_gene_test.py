1743641b67a3c815d1e4f76564b051d28970d553
Colin Targonski <ctargon@g.clemson.edu>
2017-11-30 15:22:23 -0500

    add random capabilities to subset_gene_test

--- scripts/subset_gene_test.py
+++ scripts/subset_gene_test.py
@@ -11,6 +11,7 @@ import subprocess
 import json
 import shutil
 import itertools
+import sys, argparse
 
 # special helper function to sanitize the string containing the genes from
 # an accuracy file
@@ -26,8 +27,6 @@ def sanitize(gene_str):
 # of new combinations with one extra gene appended that was not previously in the list
 def create_new_combos_from_file(file, genes):
 	combos = []
-	# for subset in itertools.combinations(genes, 2):
-	# 	combos.append(subset)
 	prev_accs = np.loadtxt(file, delimiter='\t', dtype=np.str)
 	sort = prev_accs[np.argsort(prev_accs[:,1])]
 	top_5_perc = sort[len(sort) - 15:]
@@ -88,50 +87,68 @@ def create_subset(sub_gene, tot_gene_lists):
 		sub_sample = sub_sample.astype(np.float32)
 		#print(len(random_sample))
 		sub_sample.tofile("../datasets/GTEx_Notch/"+ tissues[i] + '/' + tissue_samples[j])
+
+def create_random_subset(num_genes, tot_gene_lists):		
+	#Generate Gene Indexes for Random Sample
+	gene_indexes = np.random.randint(0, 56238, num_genes)
+	return [tot_gene_lists[i] for i in gene_indexes]
+
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Run tests on specified subsets of a hallmark or random set')
+	parser.add_argument('--set', help='subset to be used', type=str, required=True, choices=['hedgehog', 'notch', 'random'])
+	parser.add_argument('--num_genes', help='number of genes', type=int, required=True)
+	args = parser.parse_args()
+
+	gtex_str_data = np.load('../datasets/gtex_gct_data_string.npy')
+	total_gene_list = gtex_str_data[1:,1]
+
+	# load the hedgehog data
+	if args.set == 'hedgehog':
+		sub = np.load('../datasets/hallmark_numpys/HALLMARK_HEDGEHOG_SIGNALING.npy')
+		genes = sub[:,1].tolist()
+	elif: args.set == 'notch':
+		sub = np.load('../datasets/hallmark_numpys/HALLMARK_NOTCH_SIGNALING.npy')
+		genes = sub[:,1].tolist()
+	else:
+		genes = create_random_subset(args.num_genes, total_gene_list)
+
+	for i in xrange(1, len(genes)):
+
+		print('--------ITERATION ' + str(i) + '--------')
+		# read in the previous accuracy file
+		if i > 3:
+			gene_dict = create_new_combos_from_file('../logs/notch/notch_' + str(i) + '_gene_accuracy.txt', genes)
+		else:
+			gene_dict = create_raw_combos(genes, i)
+
+		# collect the data from the hedgehog file
+		data = sub[:,2:] # raw data is in 2
+
+		# create files to write to, specify neural net architecture
+		files = ['notch_' + str(i + 1) + '_gene_accuracy.txt']
 		
+		h1 = [1024]
+		h2 = [1024]
+		h3 = [1024]
 
-# load the hedgehog data
-sub = np.load('../datasets/hallmark_numpys/HALLMARK_NOTCH_SIGNALING.npy')
-genes = sub[:,1].tolist()
+		fp = open('../logs/notch/' + files[0], 'w')
 
-# collect the total list of genes in the GTEx dataset
-gtex_str_data = np.load('../datasets/gtex_gct_data_string.npy')
-total_gene_list = gtex_str_data[1:,1]
+		for key in gene_dict:
+			# retrieve the new combination of genes and create a new dataset containing the specified features
+			combo = list(key)
+			create_subset(combo, total_gene_list)
 
-for i in xrange(1, 4):
+			# partition the newly created datset into a training and test set
+			os.system('python ../data_scripts/create-sets.py -d gtex -p ../datasets/GTEx_Notch ' + ' -t 70 -r 30 ')
 
-	print('--------ITERATION ' + str(i) + '--------')
-	# read in the previous accuracy file
-	#gene_dict = create_new_combos_from_file('../logs/hedgehog_' + str(i) + '_gene_accuracy.txt', genes)
+			# run the neural network architecture to retrieve an accuracy based on the new dataset
+			acc = subprocess.check_output('python ../models/nn_gtex.py --n_input ' + str(i + 1) + \
+				' --n_classes 53 --batch_size 256 --lr 0.001 --epochs 75 --h1 ' + str(h1[0]) + ' --h2 ' + str(h2[0]) + ' --h3 ' + str(h3[0]), shell=True)
 
-	gene_dict = create_raw_combos(genes, i)
+			print('iteration ' + str(i) + ' ' + str(combo) + '\t' + str(acc))
+			
+			fp.write('{0}\t{1}\n'.format(key, acc))
 
-	# collect the data from the hedgehog file
-	data = sub[:,2:] # raw data is in 2
-
-	# create files to write to, specify neural net architecture
-	files = ['notch_' + str(i + 1) + '_gene_accuracy.txt']
-	
-	h1 = [1024]
-	h2 = [1024]
-	h3 = [1024]
-
-	fp = open('../logs/notch/' + files[0], 'w')
-
-	for key in gene_dict:
-		# retrieve the new combination of genes and create a new dataset containing the specified features
-		combo = list(key)
-		create_subset(combo, total_gene_list)
-
-		# partition the newly created datset into a training and test set
-		os.system('python ../data_scripts/create-sets.py -d gtex -p ../datasets/GTEx_Notch ' + ' -t 70 -r 30 ')
-
-		# run the neural network architecture to retrieve an accuracy based on the new dataset
-		acc = subprocess.check_output('python ../models/nn_gtex.py --n_input ' + str(i) + \
-			' --n_classes 53 --batch_size 256 --lr 0.001 --epochs 75 --h1 ' + str(h1[0]) + ' --h2 ' + str(h2[0]) + ' --h3 ' + str(h3[0]), shell=True)
-
-		print('iteration ' + str(i) + ' ' + str(combo) + '\t' + str(acc))
-		
-		fp.write('{0}\t{1}\n'.format(key, acc))
-
-	fp.close() 
+		fp.close() 
