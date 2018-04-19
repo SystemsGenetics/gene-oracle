9debc5bbae37b4317c59c04ea466ada0ac0220c4
Colin Targonski <ctargon@g.clemson.edu>
2018-04-19 19:05:10 -0400

    graphing scripts update

--- viz/delta_accs_box.py
+++ viz/delta_accs_box.py
@@ -24,15 +24,19 @@ import pandas as pd
 import collections
 
 # read in the accuracy files
-def read_file_avg(file):
+def read_fileAvg(file):
 	avg_accs = {}
 	with open(file, 'r') as f:
 		next(f) # skip the first line which has string header info
 		for line in f:
 			line = line.split('\t')
-			avg_accs[line[0]] = float(line[1])
+			if line[0] not in avg_accs:
+				avg_accs[line[0]] = [float(line[1])]
+			else:
+				avg_accs[line[0]].append(float(line[1]))
 
 	return avg_accs
+
 
 def read_file(file):
 	accs = {}
@@ -46,7 +50,11 @@ def read_file(file):
 			info.append(float(line[1]))#avg
 			info.append(float(line[3]))#max
 			info.append(float(line[4]))#min
-			accs[line[0]] = info
+
+			if line[0] not in accs:
+				accs[line[0]] = [info]
+			else:
+				accs[line[0]].append(info)
 
 	return accs
 
@@ -82,20 +90,20 @@ def plotDouble(ran_accs, sub_accs,out):
 	sub_data = sub_accs.values()
 	#fixed_data_sub = list(sub_data)
 
-	print(len(sub_data))
-	print(len(rand_data))
+	#print(len(sub_data))
+	#print(len(rand_data))
 
 	fig, ax = plt.subplots()
 	fig.set_size_inches(20,10)
 
 	pos = np.arange(len(rand_data))+.2 
 	ran_bp = ax.boxplot(rand_data,positions=pos,vert =False,widths=.25, 
-                 patch_artist=True, boxprops=dict(facecolor="red",edgecolor='red',alpha = .5))
+                 patch_artist=True, boxprops=dict(edgecolor='red',alpha = .5),showfliers=False)
 	plt.setp(ran_bp['whiskers'], color="red")
 
 	pos = np.arange(len(sub_data))-.2 
 	sub_bp = ax.boxplot(sub_data, vert = False,positions=pos,widths=.25, 
-                 patch_artist=True, boxprops=dict(edgecolor='blue',alpha=.3))
+                 patch_artist=True, boxprops=dict(edgecolor='blue',alpha=.3),showfliers=False)
 	plt.setp(sub_bp['whiskers'], color="blue")
 
 	ax.legend([ran_bp["boxes"][0], sub_bp["boxes"][0]], ['random', 'hallmark'], loc='upper right')
@@ -139,36 +147,36 @@ if __name__ == '__main__':
 	#print(gene_count_dict)
 
 
-	# get the delta accs
-	gene_counts = []
-	delta_accs = collections.OrderedDict()
-	for k,v in sorted(gene_count_dict.items(), key =lambda x: x[1]):
-		delta_accs[k] = []
-		current_gene_count = rand_accs[str(v)]
+	# # get the delta accs
+	# gene_counts = []
+	# delta_accs = collections.OrderedDict()
+	# for k,v in sorted(gene_count_dict.items(), key =lambda x: x[1]):
+	# 	delta_accs[k] = []
+	# 	current_gene_count = rand_accs[str(v)]
 
-		for i in range(len(current_gene_count)):
-			delta_accs[k].append(round(sub_accs[k][i] - current_gene_count[i], 3))
+	# 	for i in range(len(current_gene_count)):
+	# 		delta_accs[k].append(round(sub_accs[k][i] - current_gene_count[i], 3))
 
 
-		print(str(gene_count_dict[k]) +" : " + str(k) + " : " + str(delta_accs[k]))
+	# 	print(str(gene_count_dict[k]) +" : " + str(k) + " : " + str(delta_accs[k]))
 
-	##plotDelta(delta_accs, gene_counts, args.out)
+	# ##plotDelta(delta_accs, gene_counts, args.out)
 
 	gene_counts = []
 	rand_dict = collections.OrderedDict()
 	sub_dict = collections.OrderedDict()
+
 	for k,v in sorted(gene_count_dict.items(), key =lambda x: x[1]):
+
 		rand_dict[k] = []
 		sub_dict[k] = []
-		current_gene_count = rand_accs[str(v)]
 
-		for i in range(len(current_gene_count)):
-			rand_dict[k].append(current_gene_count[i])
-			sub_dict[k].append(sub_accs[k][i])#append(round(sub_accs[k][i] - current_gene_count[i], 3))
+		rand_dict[k].append(rand_accs[str(v)].pop())
+		sub_dict[k].append(sub_accs[k][0])#append(round(sub_accs[k][i] - current_gene_count[i], 3))
 
 
-		print(str(gene_count_dict[k]) +" : " + str(k) + " : " + str(rand_dict[k]))	
-		print(str(gene_count_dict[k]) +" : " + str(k) + " : " + str(sub_dict[k]))	
+		#print(str(gene_count_dict[k]) +" : " + str(k) + " : " + str(rand_dict[k]))	
+		#print(str(gene_count_dict[k]) +" : " + str(k) + " : " + str(sub_dict[k]))	
 
 
 
