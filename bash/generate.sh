8642239ab1904ccf1930dbb52a2fda4cedb05b5e
Ben Shealy <bentshermann@gmail.com>
2019-05-22 12:50:37 -0400

    Removed some unused files

--- bash/generate.sh
+++
@@ -1,4 +0,0 @@
-~/DeepGTEx/gen_pbs.sh $1
-echo > ~/DeepGTEx/qsub_list.sh
-~/DeepGTEx/sub_pbs.sh $1
-~/DeepGTEx/qsub_list.sh
--- bash/sub_pbs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((c=1; c <= $1; c++))
-do 
-echo qsub run$c.pbs >> qsub_list.sh
-done
